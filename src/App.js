99ea7ee84eeb5d122965021e7a313063505f5ab3
msp <badboyminato@gmail.com>
2024-03-14 17:29:12 +0630

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  Header,
+  Footer,
+  Features,
+  WhatGPT3,
+  Possibility,
+  Blog,
+} from './containers';
+import { Navbar, CTA, Brand } from './components';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './footer.css';
+import logo from '../../assets/logo.svg';
+
+const Footer = () => {
+  return (
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <button className="btn btn-secondary py-3">Request Early Access</button>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img
+            src={logo}
+            alt="Logo"
+          />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Yangon, Myanmar</p>
+          <p>+95-9773945849</p>
+          <p>badboyminato123@gmail.com</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        <p>© 2021 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
